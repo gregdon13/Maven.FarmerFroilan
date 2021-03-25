0392843e91b686119ec1c45d855abbd898c4296b
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-24 23:01:39 -0400

    coops and stables are present

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Farm.java
@@ -1,4 +1,19 @@
 package com.zipcodewilmington.froilansfarm.Farm;
 
+import com.zipcodewilmington.froilansfarm.Animals.Chicken;
+import com.zipcodewilmington.froilansfarm.Animals.Horse;
+
+import java.util.ArrayList;
+
 public class Farm {
+    Chicken chicken = new Chicken();
+    Horse horse = new Horse();
+    ChickenCoop coopOne = new ChickenCoop(4, chicken);
+    ChickenCoop coopTwo = new ChickenCoop(4, chicken);
+    ChickenCoop coopThree = new ChickenCoop(3, chicken);
+    ChickenCoop coopFour = new ChickenCoop(4, chicken);
+
+    Stable stableOne = new Stable(4, horse);
+    Stable stableTwo = new Stable(3, horse);
+    Stable stableThree = new Stable(3, horse);
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Housing.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Housing.java
@@ -2,6 +2,12 @@ package com.zipcodewilmington.froilansfarm.Farm;
 
 import com.zipcodewilmington.froilansfarm.LivingThing;
 
-public interface Housing {
-    void store(LivingThing creature);
+import java.util.ArrayList;
+
+public abstract class Housing<E extends LivingThing> {
+    ArrayList<E> livingStorage = new ArrayList<E>();
+
+    public void store(E creature) {
+        livingStorage.add(creature);
+    }
 }
