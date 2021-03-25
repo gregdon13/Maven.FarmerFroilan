d3af9c85d27262d933529c45402e46e88f916587
KellyPorter02 <porter.kelly02@gmail.com>
2021-03-24 21:07:38 -0400

    Persons & Animal package

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Persons/Person.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington.froilansfarm.Persons;
+
+import com.zipcodewilmington.froilansfarm.Eater;
+import com.zipcodewilmington.froilansfarm.LivingThing;
+import com.zipcodewilmington.froilansfarm.NoiseMaker;
+
+public interface Person extends LivingThing, Eater, NoiseMaker{
+
+
+    }
+
