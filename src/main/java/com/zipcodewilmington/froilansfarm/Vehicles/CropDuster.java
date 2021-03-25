849c2656084cc2fad9f8e9fd226d45aecc033ddd
jorge <jorgetapia0293@gmail.com>
2021-03-25 16:03:46 -0400

    adding to aircraft

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
@@ -1,10 +1,12 @@
 package com.zipcodewilmington.froilansfarm.Vehicles;
 
+import com.zipcodewilmington.froilansfarm.Farm.CropRow;
+
 public class CropDuster extends Aircraft{
     public String makeNoise() {
         return "NnNnNn";
     }
-    public void fertilizeRow(/*CropRow row*/){
+    public void fertilizeRow(CropRow row){
         //calls to crop row and sprays tf out of crops to fertilize
     }
 }
