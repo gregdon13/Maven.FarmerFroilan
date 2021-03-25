791edd63364eb2cf3e46855bcfca6ca2518e2e28
jorge <jorgetapia0293@gmail.com>
2021-03-24 20:35:37 -0400

    added methods to noiseMaker Produce and Eater

--- src/main/java/com/zipcodewilmington/froilansfarm/Crops/Crop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crops/Crop.java
@@ -4,4 +4,7 @@ import com.zipcodewilmington.froilansfarm.EdiblePackage.EdiblePlant;
 import com.zipcodewilmington.froilansfarm.Produce;
 
 public class Crop<E extends EdiblePlant> implements Produce {
+    public void addProduce(Produce produce) {
+
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Produce.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Produce.java
@@ -1,4 +1,5 @@
 package com.zipcodewilmington.froilansfarm;
 
 public interface Produce {
+    public void addProduce(Produce produce);
 }
