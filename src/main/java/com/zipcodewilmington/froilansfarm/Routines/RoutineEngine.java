bd052d2d98dba095e038d29956af1c35a67d4817
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-24 21:40:35 -0400

    ;aksdj

--- src/main/java/com/zipcodewilmington/froilansfarm/Routines/RoutineEngine.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Routines/RoutineEngine.java
@@ -3,21 +3,17 @@ package com.zipcodewilmington.froilansfarm.Routines;
 import com.zipcodewilmington.froilansfarm.Animals.Chicken;
 import com.zipcodewilmington.froilansfarm.Animals.Horse;
 import com.zipcodewilmington.froilansfarm.EdiblePackage.ChickenFood;
+import com.zipcodewilmington.froilansfarm.EdiblePackage.EarCorn;
 import com.zipcodewilmington.froilansfarm.EdiblePackage.HorseFood;
 
 public class RoutineEngine implements DailyRoutine{
-    public boolean mount (Horse horse) {return false;}
+    public boolean mount (Horse horse) { return false;}
     public boolean dismount (Horse horse) {return false;}
     public boolean feedHorse(Horse horse, HorseFood food) {return false;}
-    public boolean feedChickens(Chicken chicken, ChickenFood chickFood) {return false;}
+    public boolean feedChickens(Chicken chicken, ChickenFood chickFood) {
+        chicken.eat(chickFood);
+        return true;}
 
-    public boolean dailies (boolean mount, boolean dismount, boolean feedHorses, boolean feedChickens) {
-        if (mount && dismount && feedHorses && feedChickens) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
 
 
