5ce1398b385ed16b7f710f47cbd121553c3354d3
jorge <jorgetapia0293@gmail.com>
2021-03-24 21:13:42 -0400

    Added method to ProduceStand

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/Housing.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/Housing.java
@@ -1,4 +1,7 @@
 package com.zipcodewilmington.froilansfarm.Farm;
 
+import com.zipcodewilmington.froilansfarm.LivingThing;
+
 public interface Housing {
+    void store(LivingThing creature);
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Farm/ProduceStand.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/ProduceStand.java
@@ -1,4 +1,9 @@
 package com.zipcodewilmington.froilansfarm.Farm;
 
+import com.zipcodewilmington.froilansfarm.Produce;
+
 public class ProduceStand {
+    public void addProduce(Produce stuffs){
+
+    }
 }
