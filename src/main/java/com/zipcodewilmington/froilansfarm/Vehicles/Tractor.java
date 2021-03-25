7d7870774abe7bdc5def7e6dd088160cf07259e7
jorge <jorgetapia0293@gmail.com>
2021-03-25 19:21:58 -0400

    more stuff

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Tractor.java
@@ -1,12 +1,12 @@
 package com.zipcodewilmington.froilansfarm.Vehicles;
 
 import com.zipcodewilmington.froilansfarm.Crops.Crop;
+import com.zipcodewilmington.froilansfarm.Farm.CropRow;
 
 public class Tractor extends FarmVehicle{
     public String makeNoise() {
         return "Clack-Clunk";
     }
-    public void harvestCrop(){
-        //method which calls to crop row and tells that its getting harvested af
+    public void harvestRow(CropRow row){
     }
 }
